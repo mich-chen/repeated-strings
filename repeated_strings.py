a61c49620297b26c31067a763e30417badbf2c20
Michelle Chen <mich.chen.94@gmail.com>
2020-10-04 17:41:46 -0700

    brute force, O(nm) solution, not ideal

--- repeated_strings.py
+++ repeated_strings.py
@@ -4,3 +4,21 @@ def repeatedString(s, n):
     if s == 'a':
         print(n)
 
+
+"""
+Alternative solution / O(nm)
+def repeatedString(s,n):
+
+    if s == 'a':
+        return n
+
+    count = 0
+    string_long = s * n
+    string = string_long[:n]
+    for char in string:
+        if char == 'a':
+            count += 1
+
+    return count
+
+"""

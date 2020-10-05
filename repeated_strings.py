847026e6162a1c2c720fd4e77961458c2f5789ba
Michelle Chen <mich.chen.94@gmail.com>
2020-10-04 17:51:58 -0700

    calculating exact 1st n letters string, still runtime O(mn)

--- repeated_strings.py
+++ repeated_strings.py
@@ -4,6 +4,22 @@ def repeatedString(s, n):
     if s == 'a':
         print(n)
 
+    count = 0
+    # remaining number of letters to complete sequence
+    remaining_string = n - len(s)
+    # number of repeats using floor division
+    num_repeats = remaining_string // len(s)
+    # calculate any remaining letters
+    remaining_letters = remaining_string % (s)
+    # concatenate everything for 1st n number of letters
+    string = s + (s * num_repeats) + s[:remaining_letters]
+
+    for char in string:
+        if char == 'a':
+            count += 1
+
+    return count
+
 
 """
 Alternative solution / O(nm)
